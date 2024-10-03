14e6d85b3f165a24d0a978b322c4c6d203938308
Gian Mateo <gclm4002@gmail.com>
2024-10-03 08:22:10 +0800

    added General Info about the group

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,16 @@ from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 
 
+#General Information About The Group
+st.write("CSS145_BM4_1Q2425")
+st.title("Group 2: ")
 
+st.markdown("""
+- Aleah Balagao
+- Sean Chester Cabantog
+- John Daniel Garina
+- Gian Carlo Mateo
+- Felipe Panugan
+""")
 
-
+#imported datasheet
